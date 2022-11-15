8b5d800d4ba979b1f53f78c6338b68c6751b922e
mikuStark <karbainovam@gmail.com>
2022-11-15 14:15:04 +0300

    правки

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,5 @@
 CREATE TABLE product (id SERIAL PRIMARY KEY, product_id VARCHAR(255), value VARCHAR(255));
+
+INSERT INTO product(id, product_id, value) VALUES ('1', '098', 'productName098'),
+                                                  ('2', '088', 'productName088'),
+                                                  ('3', '087', 'productName087');
